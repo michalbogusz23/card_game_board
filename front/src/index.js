e6b2d2f87e626320854e759cb973f54ffb88ec9d
Michal Bogusz <michal.bogusz@websensa.com>
2021-06-14 22:59:39 +0200

    feat: add dockerized react front app

---
+++ front/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    Hello world oksad
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
