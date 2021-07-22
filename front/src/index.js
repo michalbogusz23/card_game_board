05b13eeb796696e6877477dbfc00984ac79c20b7
Michal Bogusz <michal.bogusz@websensa.com>
2021-07-22 21:12:31 +0200

    feat: show basiv view of board

--- front/src/index.js
+++ front/src/index.js
@@ -2,10 +2,73 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Table extends React.Component {
+  render() {
+    return (
+      <div className="table">
+        <Player/>
+        <Board/>
+        <Player/>
+      </div>
+    )
+  }
+}
+class Player extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      cardsValues: Array(9).fill(1),
+    }
+  }
+
+  renderCard(i) {
+    return (
+      <div className="player-card"></div>
+    );
+  }
+  render() {
+    const cardsValues = this.state.cardsValues;
+
+    const cards = cardsValues.map((step, card) =>  {
+      return (
+        <div>{card}</div>
+      )
+    })
+    return (
+      <div className="player">
+        <div className="player-cards">
+          {cards}
+        </div>
+        <div className="player-button">
+
+        </div>
+      </div>
+      
+    )
+  }
+}
+class Board extends React.Component {
+  render() {
+    return (
+      <div className="board">
+        <div className="board-cards">
+          <div className="board-card-row">
+
+          </div>
+          <div className="board-card-row">
+
+          </div>
+        </div>
+        <div className="board-result">
+
+        </div>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    Hello world oksad
-  </React.StrictMode>,
+  <Table />,
   document.getElementById('root')
 );
 
