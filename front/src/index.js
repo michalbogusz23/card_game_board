3ae18a9a269f02b0088bc323f157f16f358ee923
Michal Bogusz <michal.bogusz@websensa.com>
2021-07-28 15:31:14 +0200

    refactor: changed data structure and optimized drawing cards

--- front/src/index.js
+++ front/src/index.js
@@ -6,59 +6,58 @@ class Table extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      top_cards: Array.from(Array(10)).map(x=>Math.floor(Math.random() * 11)),
-      bot_cards: Array.from(Array(10)).map(x=>Math.floor(Math.random() * 11)),
-      top_ingame_cards: [],
-      bot_ingame_cards: [],
+      player_cards: [
+        Array.from(Array(10)).map(x=>Math.floor(Math.random() * 11)),
+        Array.from(Array(10)).map(x=>Math.floor(Math.random() * 11))
+      ],
+      ingame_cards: Array.from(Array(2), () => new Array(0)),
       topIsNext: true,
     };
   }
   handleClick() {
     if(this.state.topIsNext) {
-      const top_ingame_cards = this.state.top_ingame_cards.slice();
-      const top_cards = this.state.top_cards.slice();
-      top_ingame_cards.push(top_cards.pop())
-      this.setState({
-        top_ingame_cards: top_ingame_cards,
-        top_cards: top_cards,
-        topIsNext: !this.state.topIsNext
-      })
+      this.drawCard(0);
     } else {
-      const bot_ingame_cards = this.state.bot_ingame_cards.slice();
-      const bot_cards = this.state.bot_cards.slice();
-      bot_ingame_cards.push(bot_cards.pop())
-      this.setState({
-        bot_ingame_cards: bot_ingame_cards,
-        bot_cards: bot_cards,
-        topIsNext: !this.state.topIsNext
-      })
+      this.drawCard(1);
     }
   }
+  drawCard(player) {
+    const ingame_cards = this.state.ingame_cards.slice();
+    const player_cards = this.state.player_cards.slice();
+    ingame_cards[player].push(player_cards[player].pop());
+    this.setState({
+      ingame_cards: ingame_cards,
+      player_cards: player_cards,
+      topIsNext: !this.state.topIsNext
+    });
+  }
+
   render() {
     return (
       <div className="table">
         <Player 
-          value={this.state.top_cards}
+          value={this.state.player_cards[0]}
           onClick={() => this.handleClick()}/>
         <Board
-          top={this.state.top_ingame_cards}
-          bot={this.state.bot_ingame_cards}/>
+          value={this.state.ingame_cards}/>
         <Player 
-          value={this.state.bot_cards}
+          value={this.state.player_cards[1]}
           onClick={() => this.handleClick()}/>
       </div>
     )
   }
 }
+
+
 class Player extends React.Component {
   render() {
     const values = this.props.value;
-
     const cards = values.map((card) =>  {
       return (
         <div>{card}</div>
       )
     });
+    
     return (
       <div className="player">
         <div className="player-cards">
@@ -72,10 +71,12 @@ class Player extends React.Component {
     )
   }
 }
+
+
 class Board extends React.Component {
   render() {
-    const top_player = this.props.top;
-    const bottom_player = this.props.bot;
+    const top_player = this.props.value[0];
+    const bottom_player = this.props.value[1];
 
     const top_cards = top_player.map((card) =>  {
       return (
